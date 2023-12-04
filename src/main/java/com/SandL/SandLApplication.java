363f58e1b023514b71f0c3564c2dabfe7dd16df8
Jose-MPM <jose_manuel@ciencias.unam.mx>
2023-12-03 18:39:10 -0600

    [INIT] Spring inicialaizerProject

---
+++ src/main/java/com/SandL/SandLApplication.java
@@ -0,0 +1,13 @@
+package com.SandL;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SandLApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SandLApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/SandL/SandLApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SandL;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SandLApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
